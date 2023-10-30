a77384024eb06aa08e9c0bf7bbb578e0b333280e
ThuPhuong483 <147456514+ThuPhuong483@users.noreply.github.com>
2023-10-30 21:59:06 +0700

    Update Practice_8.sql

--- Practice_8.sql
+++ Practice_8.sql
@@ -2,6 +2,15 @@
 --EX2:
 --EX3:
 --EX4:
+SELECT b.visited_on, b.amount,
+ROUND(b.amount/7,2) AS average_amount 
+FROM (SELECT a.visited_on, 
+SUM(a.amount1) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ) AS amount
+FROM (SELECT visited_on, SUM(amount) AS amount1
+FROM Customer
+GROUP BY visited_on) AS a) AS b
+WHERE b.visited_on >= (SELECT min(visited_on) FROM Customer)+6
+
 --EX5:
 
 
